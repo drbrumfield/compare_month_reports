2fd4304b40a462618912b8d26e4842738bddd1eb
Sara Brumfield <sbrumfield@howardcountymd.gov>
2024-02-26 16:13:36 -0500

    fixed import formatting for crystal report
    now matches sql ttx import format

--- crystal_compare.R
+++ crystal_compare.R
@@ -4,41 +4,50 @@ library(dplyr)
 library(magrittr)
 library(openxlsx)
 library(stringr)
-library(dataCompareR)
 library(compareDF)
+library(assertthat)
+library(tidyverse)
 
 source("C:/Users/sbrumfield/OneDrive - Howard County/Documents/GitHub Repos/bbmR/R/export_excel.R")
 
-
+# crystal generated ttx needs the quote = "" parameter to correctly gather all rows; otherwise some rows will be missing
 crystal <- read.delim("T:/SHARED/MUNIS Archives/MUNIS Scanline Files/Scanline TTX Files/Processed/Scanline-02-26-2024.ttx", header = FALSE, 
                       skip = 8, col.names = c("Year", "Category", "Bill Number", "Account", "Name", "Name2", "Addr Line 1",
                                               "Addr line 2", "City", "State", "Zip", "TotalDue"), 
-                      sep = "\t")
+                      sep = "\t", quote = "") %>%
+  # this is the only way to remove the trailing whitespace; trimws and str_trim don't work
+  # TotalDue has commas in number format
+  mutate(Account = gsub(pattern = "(\\s)", replacement = "", x = Account))
 
 python <- read.delim("T:/SHARED/MUNIS Archives/MUNIS Scanline Files/Scanline TTX Files/Scanline-02-26-2024.ttx", header = FALSE, 
                      skip = 8, col.names = c("Year", "Category", "Bill Number", "Account", "Name", "Name2", "Addr Line 1",
                                              "Addr line 2", "City", "State", "Zip", "TotalDue"), 
-                     sep = "\t") %>%
+                     sep = "\t", quote = "") %>%
   #convert data type
   mutate(TotalDue = as.character(TotalDue),
          TotalDue = case_when(TotalDue == "0" ~ "0.00",
-                              TRUE ~ TotalDue))
+                              TRUE ~ TotalDue),
+         Account = gsub(pattern = "(\\s)", replacement = "", x = Account))
 
+
+assert_that(nrow(python) == nrow(crystal))
 
 ##dplyr compare method====
 
-notinold <- anti_join(python, crystal)
-notinnew <- anti_join(crystal, python)
+notinold <- anti_join(python, crystal, by = c("Year", "Account", "Category", "Bill.Number"))
+notinnew <- anti_join(crystal, python, by = c("Year", "Account", "Category", "Bill.Number"))
 
 ##return all rows from x without a match in y
-diff1 <- anti_join(python, crystal)
-diff2 <- anti_join(crystal, python)
+diff1 <- anti_join(python, crystal, by = c("Year", "Account", "Category", "Bill.Number"))
+diff2 <- anti_join(crystal, python, by = c("Year", "Account", "Category", "Bill.Number"))
 
-alldiff <- rbind(diff1, diff2)
+alldiff <- rbind(diff1, diff2) %>%
+  arrange(`Year`, `Account`, `Category`, `Bill.Number`)
 
 ##comparedf package ===
 
-df <- compare_df(python, crystal)
+df <- compare_df(python, crystal,
+                 change_markers = c("python", "crystal"))
 
 comp_df <- df$comparison_df
 create_output_table(df, change_col_name  = "Added/Removed", group_col_name = "Line Number", output_type = "html", file_name = "C:/Users/sbrumfield/OneDrive - Howard County/Documents/SDAT Comparisons/SDAT Sep-Dec 2023 Differences.html", limit = 7000)
